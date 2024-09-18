72478131143bf4ef0b41349e8d6bf999d22d9397
Justin Bezanson <justinbezanson@gmail.com>
2024-09-17 21:52:41 -0300

    typo

--- resources/views/livewire/trips/edit-trip.blade.php
+++ resources/views/livewire/trips/edit-trip.blade.php
@@ -86,7 +86,7 @@ new class extends Component {
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Create a new trip.") }}
+            {{ __("Edit trip.") }}
         </p>
     </header>
 
